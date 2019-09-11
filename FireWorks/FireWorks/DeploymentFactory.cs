4ae65bb04b8d335f39e422f573c28b0876902e90
Moritz Pape <moritz.pape@circlon.de>
2019-09-11 17:00:46 +0200

    Added DeploymentFactory.cs
    A class to generate Deployment objects.

---
+++ FireWorks/FireWorks/DeploymentFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireWorks
+{
+    class DeploymentFactory
+    {
+        public DeploymentFactory()
+        {
+        }
+        public Deployment NewDeployment()
+        {
+            Console.WriteLine("Where?");
+            string Loc = Console.ReadLine();
+            Console.WriteLine("Which vehicles were used?");
+            string Veh = Console.ReadLine();
+            Console.WriteLine("What resources were used?");
+            string Res = Console.ReadLine();
+            Console.WriteLine("Who was send?");
+            string Hum = Console.ReadLine();
+            Console.WriteLine("Comments?");
+            string Com = Console.ReadLine();
+            return new Deployment(Loc, Veh, Res, Hum, Com);
+        }
+    }
+}
