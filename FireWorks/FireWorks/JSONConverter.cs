3352a72807e2647db3130462e8198548694e789d
Moritz Pape <moritz.pape@circlon.de>
2019-09-11 18:16:16 +0200

    Added JSON conversation
    Integrated Newtonsoft.Json;
    Created Wrapper for ease of use.

---
+++ FireWorks/FireWorks/JSONConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace FireWorks
+{
+    class JSONConverter
+    {
+        public static string ObjectToJSON(object o)
+        {
+            return JsonConvert.SerializeObject(o, Formatting.None);
+        }
+        public static object JSONToObject(string str)
+        {
+            return JsonConvert.DeserializeObject<Deployment>(str);
+        }
+    }
+}
