e2abd6d5ed45a96a8a5557c5be0ef1a66aa4ff9f
Moritz Pape <moritz.pape@circlon.de>
2019-09-11 14:32:11 +0200

    Added Deployment.cs / tidied up Authenticator.cs
    Added the class Deployment which will be the Object to hold the information
    needed to create a Deployment.
    
    Tieded the code for Authenticator.cs

---
+++ FireWorks/FireWorks/Deployment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireWorks
+{
+    class Deployment
+    {
+        public string DateAndTime { get; set; }
+        public string Location { get; set; }
+        public object Vehicles { get; set; }
+        public object Resources { get; set; }
+        public object Human { get; set; }
+        public string Comment { get; set; }
+        public int Number { get; set; }
+        private static int counter = 1;
+        public Deployment() //null
+        {
+
+        }
+        public Deployment(string Loc, object Veh, object Res, object Hum, string Com )
+        {
+            DateAndTime = DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+            Location = Loc;
+            Vehicles = Veh;
+            Resources = Res;
+            Human = Hum;
+            Comment = Com;
+            Number = counter;
+            counter++;
+        }
+    }
+}
