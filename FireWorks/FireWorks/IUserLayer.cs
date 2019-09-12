aa45572af2c1abf042691a028413442c61adea2c
Moritz Pape <moritz.pape@circlon.de>
2019-09-12 19:54:01 +0200

    Added TUI.cs and IUserLayer.cs
    Interface and a class that implements it to be able to migrate to a GUI.

---
+++ FireWorks/FireWorks/IUserLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireWorks
+{
+    interface IUserLayer
+    {
+        void Display(string str);
+        int GetInt();
+
+        string GetString();
+        bool GetBool();
+
+
+
+    }
+}
---
+++ FireWorks/FireWorks/TUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireWorks
+{
+    class TUI : IUserLayer
+    {
+        public TUI()
+        {
+
+        }
+        public void Display(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public int GetInt()
+        {
+            Console.WriteLine("Type a number");
+            string Input = Console.ReadLine();
+            for (int i = 0; i < Input.Length; i++)
+            {
+                if (!char.IsDigit(Input[i]))
+                {
+                    return GetInt();
+                }
+            }
+            return int.Parse(Input);
+        }
+        public string GetString()
+        {
+            return Console.ReadLine();
+        }
+        public bool GetBool()
+        {
+            Console.WriteLine("(y/n)?");
+            string str = Console.ReadLine();
+            if ((str == "y") || (str == "Y") || (str == "Yes") || (str == "yes"))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
