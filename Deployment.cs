0c9a38b3a63620e35d3d45bb8d60711208b892a6
KilianHof <55136002+KilianHof@users.noreply.github.com>
2019-09-12 13:52:47 +0200

    Added Deployment listing

--- Deployment.cs
+++ Deployment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 
-namespace Fireworks
+namespace FireWorks
 {
 
 
@@ -20,4 +20,66 @@ namespace Fireworks
         public string Comment { get; set; } // Kommentar
         public int Number { get; set; } // Einsatz-ID
     }
+
+    public class DeploymentListing
+    {
+        public static void DeploymentList()
+        {
+
+            int i = 0;
+            Console.WriteLine("Wie viele Einsätze sollen Maximal gezeigt werden?");
+            int.TryParse(Console.ReadLine(), out int number);
+            number += 1;
+            string read;
+            String SDeployments = "";
+            using (StreamReader UserText = new StreamReader(@"C:/Users/khof/Desktop/Deployments.txt"))
+          
+
+                while (SDeployments != null)
+                {
+
+
+                   SDeployments  = UserText.ReadLine();
+
+                    i += 1;
+
+                }
+
+                Deployment[] Deployments = new Deployment[i];
+            i = 0;
+            SDeployments = "";
+            using (StreamReader UserText = new StreamReader(@"C:/Users/khof/Desktop/Deployments.txt"))
+         
+                while (SDeployments != null)
+                {
+
+
+                    SDeployments = UserText.ReadLine();
+                    if (SDeployments != null)
+                    {
+                        Deployments[i] = JsonConvert.DeserializeObject<Deployment>(SDeployments);
+                    }
+                    i += 1;
+                    
+                }
+            i -= 1;
+            int iMax = i;
+            number -= 2;
+            while (number > -1)
+            {
+                if (number > iMax)
+                {
+                    Console.WriteLine("Es sind nur "+iMax+" Einträge vorhanden.");
+                    DeploymentList();
+                    break;
+                }
+                Console.WriteLine("Datum: "+Deployments[number].Date + "    Ort: "+Deployments[number].Location+"    ID:"+Deployments[number].Number);
+                number -= 1;
+            }
+
+            return;
+            
+        }
+    }
+
 }
--- MainFireWorks.cs
+++ MainFireWorks.cs
@@ -10,11 +10,19 @@ namespace FireWorks
 {
     class MainFireWorks
     {
+#pragma warning disable IDE0060 // Nicht verwendete Parameter entfernen
         public static void Main(string[] args)
+#pragma warning restore IDE0060 // Nicht verwendete Parameter entfernen
         {
 
 
-
+            Deployment test = new Deployment();
+            string testtext;
+            using (StreamReader UserText = new StreamReader(@"C:/Users/khof/Desktop/Deployments.txt"))
+            testtext = UserText.ReadLine();
+                    test = JsonConvert.DeserializeObject<Deployment>(testtext);
+            Console.WriteLine(test.Location+test.Date);
+                
 
             Human CurrentUser = new Human();
             var SCurrentUser = Authenticator.LogIn();
@@ -24,7 +32,16 @@ namespace FireWorks
 
             Authenticator.StatusCheck(CurrentUser);
 
-           
+            Console.Clear();
+            Console.WriteLine("(e)insatzliste / Einsatz(s)uche");
+            String Answer = Console.ReadLine();
+
+           if (Answer == "e")
+            {
+                DeploymentListing.DeploymentList();
+            }
+
+            Console.ReadLine();
 
         }
     }
