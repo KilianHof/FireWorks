3f9c454ee99ed40c1a25ade9b19c8cc1bb8344ed
Moritz Pape <moritz.pape@circlon.de>
2019-09-12 10:34:07 +0200

    Overall bugfixes and improvements.
    Added packages.config
    Added FileWriter.cs
    Tidied up DeploymentFactory.cs
    Changed Deployment.Number to be tracked by creation.

---
+++ FireWorks/FireWorks/FileWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FireWorks
+{
+    class FileWriter
+    {
+        public static void WriteToFile(object o, string path)
+        {
+            string Json = JSONConverter.ObjectToJSON(o) + Environment.NewLine;
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.AppendAllText(path, Json);
+                }
+                else
+                {
+                    using (FileStream Fs = File.Create(path))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes(Json);
+                        Fs.Write(info, 0, info.Length);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Couldnt read and/or create the File.(" + path + ")");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
