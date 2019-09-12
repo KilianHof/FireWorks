ee4a946da661e596ab6b033eca3138bac87c8317
Moritz Pape <moritz.pape@circlon.de>
2019-09-12 19:57:22 +0200

    Added Resources.cs and Hose.cs
    Helper classes.

---
+++ FireWorks/FireWorks/Hose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireWorks
+{
+    public class Hose : Resources
+    {
+        public char Letter { get; set; }
+        public Hose(string d, int inn, char l) :
+            base(d, inn)
+        {
+            Letter = l;
+        }
+    }
+}
---
+++ FireWorks/FireWorks/Resources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireWorks
+{
+    public class Resources
+    {
+        public string Description { get; set; }
+        public int InventoryNumber { get; set; }
+
+
+        public Resources(string d, int inn)
+        {
+            Description = d;
+            InventoryNumber = inn;
+        }
+    }
+}
